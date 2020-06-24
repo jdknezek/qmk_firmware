6773d933ec323ca727e0de3e32ce9326ba5cd844
Jonathan Knezek <jdknezek@gmail.com>
2020-06-24 10:32:28 -0500

    Use KC_P* codes

--- keyboards/preonic/keymaps/jdknezek/keymap.c
+++ keyboards/preonic/keymaps/jdknezek/keymap.c
@@ -58,11 +58,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         _______, KC_MINS, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______  \
    ),
     [_LOWER] = LAYOUT_preonic_grid( \
-        _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   _______, _______, KC_KP_7, KC_KP_8,   KC_KP_9,     KC_KP_ASTERISK, _______, \
-        _______, KC_F5,   KC_F6,   KC_F7,   KC_F8,   _______, _______, KC_KP_4, KC_KP_5,   KC_KP_6,     KC_KP_MINUS,    _______, \
-        _______, KC_F9,   KC_F10,  KC_F11,  KC_F12,  _______, _______, KC_KP_1, KC_KP_2,   KC_KP_3,     KC_KP_PLUS,     _______, \
-        _______, _______, _______, _______, _______, _______, _______, KC_KP_0, KC_KP_DOT, KC_KP_SLASH, KC_KP_ENTER,    _______, \
-        _______, _______, _______, _______, _______, _______, _______, _______, KC_HOME,   KC_PGDN,     KC_PGUP,        KC_END   \
+        _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   _______, _______, KC_KP_7, KC_KP_8, KC_KP_9, KC_PAST, _______, \
+        _______, KC_F5,   KC_F6,   KC_F7,   KC_F8,   _______, _______, KC_KP_4, KC_KP_5, KC_KP_6, KC_PMNS, _______, \
+        _______, KC_F9,   KC_F10,  KC_F11,  KC_F12,  _______, _______, KC_KP_1, KC_KP_2, KC_KP_3, KC_PPLS, _______, \
+        _______, _______, _______, _______, _______, _______, _______, KC_KP_0, KC_PDOT, KC_PSLS, KC_PENT, _______, \
+        _______, _______, _______, _______, _______, _______, _______, _______, KC_HOME, KC_PGDN, KC_PGUP, KC_END   \
     ),
     [_RAISE] = LAYOUT_preonic_grid( \
         KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  _______, \
