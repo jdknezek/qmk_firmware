5437800e2b7bbeda11dbd99770520d020b9ebcfc
Jonathan Knezek <jdknezek@gmail.com>
2020-06-24 07:50:48 -0500

    Updated makestuff

--- make.cmd
+++ make.cmd
@@ -1,1 +1,1 @@
-docker run --rm -it -w /qmk_firmware -v %CD%:/qmk_firmware:rw -e ALT_GET_KEYBOARDS=true -e SKIP_GIT=true qmkfm/base_container make %1
+docker run --rm -it -w /qmk_firmware -v %CD%:/qmk_firmware:rw -e ALT_GET_KEYBOARDS=true -e SKIP_GIT=false qmkfm/base_container make %1
